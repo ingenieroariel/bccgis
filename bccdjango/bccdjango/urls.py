83c9bcd4a71a03982a9e4da49d56d96bfd1626d0
Ariel Nunez <ingenieroariel@gmail.com>
2015-10-14 11:10:17 +0600

    Added banks endpoint

--- bccdjango/bccdjango/urls.py
+++ bccdjango/bccdjango/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from osm.views import index
+from osm.views import index, banks
 
 urlpatterns = [
     url(r'^$', index),
+    url(r'^banks$', banks),
     url(r'^admin/', include(admin.site.urls)),
 ]
 
--- bccdjango/osm/views.py
+++ bccdjango/osm/views.py
@@ -4,3 +4,7 @@ from django.http import HttpResponse
 def index(request):
     html = '<html><body>Hello Bangladesh</body></html>'
     return HttpResponse(html)
+
+def banks(request):
+    html = '<html><body>This is the list of banks</body></html>'
+    return HttpResponse(html)
