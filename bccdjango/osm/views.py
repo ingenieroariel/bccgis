050223d5e821976a8478bc9e6c9674195965fd03
Ariel Nunez <ingenieroariel@gmail.com>
2015-10-14 12:14:09 +0600

    List of banks

--- bccdjango/osm/views.py
+++ bccdjango/osm/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from osm.models import Bank
 
 def index(request):
     html = '<html><body>Hello Bangladesh</body></html>'
     return HttpResponse(html)
 
 def banks(request):
-    html = '<html><body>This is the list of banks</body></html>'
+    html = '<html><body>'
+    html += '<h1>List of banks</h1>'
+    html += '<ul>'
+
+    for bank in Bank.objects.all():
+        # We do not want to display the banks with no name.
+        if bank.name is None:
+            continue
+
+        # Add the bank's name and location to the webpage.
+        html += '<li>%s: (%s, %s)</li>' % (bank.name, 0, 0)
+
+    html += '</ul></body></html>'
     return HttpResponse(html)
